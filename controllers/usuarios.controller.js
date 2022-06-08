965b02d9d84bc5a4a5bdcef7d15cf01b4f68b5d0
Momfus <momfus@outlook.com>
2022-06-08 18:00:44 -0300

    Clase 109 - Validar que el campo del email sea único

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 
 const Usuario = require('../models/usuario.model');
 
@@ -16,20 +17,43 @@ const getUsuarios = async (req, res) => {
    } );
 
 };
-const crearUsuario = async(req, res) => {
+const crearUsuario = async(req, res = response ) => { // "response" se trae de libreria de express para que tenga los autocompletados generales (como los usados en el catch de más abajo)
 
    const { email, password, nombre } = req.body;
 
-   const usuario = new Usuario( req.body );
+   // Manejo de errores generales
+   try {
 
-   await usuario.save(); // Con await se espera que la promesa save se termine primero (para usarlo debe estar en una función async)
+      // Manejar errores como que hay un usuario duplicado por un campo único (email)
+      const existeEmail = await Usuario.findOne({ email }); // Trae el campo email del body
 
-   res.json( {
+      if( existeEmail ) {
+         return res.status(400).json({
+            ok: false,
+            msg: "El correo ingresado ya esta registrado"
+         })
+      }
 
-      ok: true,
-      usuario // colocarlo solo, si se llaman igual, es lo mismo que colocar usuario: usuario
+      const usuario = new Usuario( req.body );
 
-   } );
+      await usuario.save(); // Con await se espera que la promesa save se termine primero (para usarlo debe estar en una función async)
+   
+      res.json( {
+   
+         ok: true,
+         usuario // colocarlo solo, si se llaman igual, es lo mismo que colocar usuario: usuario
+   
+      } );
+
+   } catch(error) {
+      console.log(error);
+      res.status(500).json({
+         ok: false,
+         msg: 'Error inesperado. Revisar logs'
+      })
+
+   }
+
 
 };
 
