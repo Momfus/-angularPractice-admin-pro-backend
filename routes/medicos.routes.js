9098266bde726b0686beb1388cc3cffd09589ef8
Momfus <momfus@outlook.com>
2022-06-14 18:49:08 -0300

    Clase 124 - Crud y modelo de médicos

---
+++ routes/medicos.routes.js
@@ -0,0 +1,41 @@
+/*
+
+   Medicos
+   Ruta: '/api/Medicos
+
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator'); // Para el paquete de validaciones se instala npm i express-validator
+const {validarCampos} = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jsw');
+
+const {
+   getMedicos,
+   crearMedico,
+   actualizarMedico,
+   borrarMedico
+} = require('../controllers/medicos.controllers');
+
+const router = Router();
+
+router.get('/', getMedicos);
+
+router.post('/', 
+   [ // Middlewares
+   ],
+   crearMedico
+);
+
+router.put('/:id', 
+   [
+
+   ], 
+   actualizarMedico
+);
+
+router.delete('/:id', 
+   borrarMedico 
+);
+
+module.exports = router;
