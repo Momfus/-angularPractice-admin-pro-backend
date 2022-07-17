2aa20195fcf2bb757ca3d6a9227411b32ebe1889
Momfus <momfus@outlook.com>
2022-07-17 17:01:19 -0300

    clase 131 - busqueda general en todas las colecciones de forma simultanea

--- controllers/busquedas.controller.js
+++ controllers/busquedas.controller.js
@@ -1,17 +1,30 @@
 
 // Metodo getTodo (requiere token) imprimir la busqueda en console log
 
+const { response } = require('express');
+const Usuario = require('../models/usuario.model');
+const Medico = require('../models/medico.model');
+const Hospital = require('../models/hospital.model');
 
 // Obtener datos que coincidan con cierto parámetro
 
-const getTodo = async (req, res) => {
+const getTodo = async (req, res = response) => {
 
    const busqueda = req.params.busqueda;
+   const regex = new RegExp( busqueda, 'i' ); // i = insensible (agregar todas las banderas)
+
+   /// Hacer todas las peticiones de una vez y destructurarlas en orden (de esa manera no hacerlo uno por uno cada búsqueda, es más eficiente)
+   const [usuarios, medicos, hospitales ] = await Promise.all([
+      Usuario.find({  nombre: regex  }),
+      Medico.find({  nombre: regex  }),
+      Hospital.find({  nombre: regex  }),
+   ]);
 
    res.json({
       ok:true,
-      msg: 'getTodo',
-      busqueda
+      usuarios,
+      medicos,
+      hospitales
    });
 
 };
