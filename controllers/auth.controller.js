18c420fdf2f3fcfbef027fa6fe541dbae150fedb
Momfus <momfus@outlook.com>
2022-12-09 15:58:44 -0300

    cargar el menu - clase 242

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -122,7 +122,7 @@ const renewToken = async(req, res = response ) => {
       ok: true,
       token,
       usuario,
-      menu: getMenuFrontEnd( usuarioDB.role )
+      menu: getMenuFrontEnd( usuario.role )
    });
 
 };
